599d05bb109fc46da65ae25567d406fbc58c9bf7
Romi Padam <romi.padam@oracle.com>
2026-05-22 18:42:00 -0700

    Connect contact form endpoint

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.CONTACT_ENDPOINT_URL = "PASTE_YOUR_PRIVATE_CONTACT_ENDPOINT_HERE";
+window.CONTACT_ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbwARFt9Z1ZClnC21HdYzfFpRJlXi2RzOtC5I1sWus6WzCq352a9coffl1WJN8m63Xgs8Q/exec";
